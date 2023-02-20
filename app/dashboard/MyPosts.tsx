b008e9f93eda861403b1ce11241258cb3f2a976e
Ollie <ollezemail@gmail.com>
2023-02-20 00:27:51 +0000

    third

--- app/dashboard/MyPosts.tsx
+++ app/dashboard/MyPosts.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query"
 import axios from 'axios'
-import { AuthPosts } from "../types/authPosts"
+import { AuthPosts } from "../types/AuthPosts"
 import EditPost from "./EditPost"
 
 const fetchAuthPosts = async () => {
